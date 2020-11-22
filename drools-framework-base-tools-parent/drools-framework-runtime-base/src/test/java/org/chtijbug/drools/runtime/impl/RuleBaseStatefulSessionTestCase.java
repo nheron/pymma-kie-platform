bdaf46e98853f73166da4d7d51cb5f17e1862724
nheron <nicolas.heron@pymma-software.com>
2020-11-22 15:40:52 +0100

    correction

--- drools-framework-base-tools-parent/drools-framework-runtime-base/src/test/java/org/chtijbug/drools/runtime/impl/RuleBaseStatefulSessionTestCase.java
+++ drools-framework-base-tools-parent/drools-framework-runtime-base/src/test/java/org/chtijbug/drools/runtime/impl/RuleBaseStatefulSessionTestCase.java
@@ -67,37 +67,5 @@ public class RuleBaseStatefulSessionTestCase {
     }
 
 
-    public class DummyFact {
-        private String name;
-        private DummyFact property;
-        private List<DummyFact> listOfFacts;
-
-        DummyFact(String name) {
-            this.name = name;
-        }
-
-        DummyFact(String name, DummyFact property) {
-            this(name);
-            this.property = property;
-        }
-
-        DummyFact(String name, DummyFact property, List<DummyFact> listOfFacts) {
-            this(name, property);
-            this.listOfFacts = listOfFacts;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public DummyFact getProperty() {
-            return property;
-        }
-
-        public List<DummyFact> getListOfFacts() {
-            return listOfFacts;
-        }
-
-    }
 
 }
